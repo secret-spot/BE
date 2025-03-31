a5c1ddc3d0f8714bba393cf14d73afd507d2108d
ruddmslee <dlruddms326@naver.com>
2025-04-01 06:21:29 +0900

    Initial commit

---
+++ src/test/java/com/example/SecretSpot/S2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SecretSpot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
