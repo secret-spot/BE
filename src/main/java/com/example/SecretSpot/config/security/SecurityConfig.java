a615b814ea39d398abad3a3d697c46fff8ebbe1d
ruddmslee <dlruddms326@naver.com>
2025-04-05 06:05:49 +0900

    Feat: SecurityConfig 작성

--- src/main/java/com/example/SecretSpot/config/security/SecurityConfig.java
+++ src/main/java/com/example/SecretSpot/config/security/SecurityConfig.java
@@ -1,4 +1,22 @@
 package com.example.SecretSpot.config.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/css/**", "/images/**", "/js/**").permitAll()
+                .anyRequest().authenticated())
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl("/api/home", true));
+            return http.build();
+    }
 }
