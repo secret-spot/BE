54714138b0cdedc2178f554e4fc320e3f2a89544
ruddmslee <dlruddms326@naver.com>
2025-04-08 12:45:39 +0900

    Feat: Repository 추가/ 수정

--- src/main/java/com/example/SecretSpot/repository/GuideRepository.java
+++ src/main/java/com/example/SecretSpot/repository/GuideRepository.java
@@ -1,9 +1,9 @@
 package com.example.SecretSpot.repository;
 
-import com.example.SecretSpot.domain.User;
+import com.example.SecretSpot.domain.Guide;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface GuideRepository extends JpaRepository<Guide, Integer> {
 }
