aa0d545c4e7f00431cf5a41fb4f1e1289dbe3e8f
ruddmslee <167386241+ruddmslee@users.noreply.github.com>
2025-04-09 16:00:34 +0900

    Revert "[Feat] BaseTimeEntity 추가"

--- src/main/java/com/example/SecretSpot/repository/UserRepository.java
+++ src/main/java/com/example/SecretSpot/repository/UserRepository.java
@@ -1,9 +1,7 @@
 package com.example.SecretSpot.repository;
 
-import com.example.SecretSpot.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public class UserRepository {
 }
