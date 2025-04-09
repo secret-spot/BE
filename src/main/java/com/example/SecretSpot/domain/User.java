b7be148a230983e431a13f88a7436a43dd996fd7
ruddmslee <dlruddms326@naver.com>
2025-04-09 13:19:58 +0900

    Feat: 브랜치 교체 위한 임시 커밋

--- src/main/java/com/example/SecretSpot/domain/User.java
+++ src/main/java/com/example/SecretSpot/domain/User.java
@@ -1,4 +1,19 @@
 package com.example.SecretSpot.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column
+    private Role role;
 }
--- src/main/java/com/example/SecretSpot/web/controller/UserController.java
+++ src/main/java/com/example/SecretSpot/web/controller/UserController.java
@@ -1,4 +1,20 @@
 package com.example.SecretSpot.web.controller;
 
+import com.example.SecretSpot.domain.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.naming.AuthenticationNotSupportedException;
+import java.util.Map;
+
+@Controller
 public class UserController {
+
+    @GetMapping("/api/mypage/profile")
+    public String profile(@AuthenticationPrincipal User user) {
+        return null;
+    }
 }
