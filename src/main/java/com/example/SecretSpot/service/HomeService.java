5c227ec2222553840d2c3512da1deaab51233319
ruddmslee <dlruddms326@naver.com>
2025-04-10 09:12:21 +0900

    Feat: 최근 게시물 불러오기 기능 추가

---
+++ src/main/java/com/example/SecretSpot/service/HomeService.java
@@ -0,0 +1,28 @@
+package com.example.SecretSpot.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class HomeService {
+//    GuideRepository guideRepository;
+//
+//    public List<Guide> getLatestGuide() {
+        // guideRepository에 코드 추가, Guide가 BaseTimeEntity 상속 필요
+//        return guideRepository.findTop5ByOrderByCreatedDateDesc();
+//    }
+//
+//    public List<Guide> getRecommendedGuide(@AuthenticationPrincipal User user) {
+         // 유저키워드 랜덤으로 뽑아서 가이드 가져오기
+//        return List.of(null);
+//    }
+//
+//    public List<Region> getRecommendedRegion() {
+         // 로직 추가 필요
+//        return List.of(null);
+//    }
+}
