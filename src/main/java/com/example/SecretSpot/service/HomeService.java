eee8635f171d4dd7869b2f32755b46e00f41c56a
ruddmslee <dlruddms326@naver.com>
2025-04-10 23:49:25 +0900

    Feat: 홈 화면 조회 주석 제거

--- src/main/java/com/example/SecretSpot/service/HomeService.java
+++ src/main/java/com/example/SecretSpot/service/HomeService.java
@@ -1,7 +1,10 @@
 package com.example.SecretSpot.service;
 
+import com.example.SecretSpot.domain.Guide;
+import com.example.SecretSpot.domain.Region;
+import com.example.SecretSpot.domain.User;
+import com.example.SecretSpot.repository.GuideRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,20 +12,19 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class HomeService {
-//    GuideRepository guideRepository;
-//
-//    public List<Guide> getLatestGuide() {
-        // guideRepository에 코드 추가, Guide가 BaseTimeEntity 상속 필요
-//        return guideRepository.findTop5ByOrderByCreatedDateDesc();
-//    }
-//
-//    public List<Guide> getRecommendedGuide(@AuthenticationPrincipal User user) {
-         // 유저키워드 랜덤으로 뽑아서 가이드 가져오기
-//        return List.of(null);
-//    }
-//
-//    public List<Region> getRecommendedRegion() {
-         // 로직 추가 필요
-//        return List.of(null);
-//    }
+    GuideRepository guideRepository;
+
+    public List<Guide> getLatestGuide() {
+        return guideRepository.findTop5ByOrderByCreatedAtDesc();
+    }
+
+    public List<Guide> getRecommendedGuide(User user) {
+        // 유저키워드 랜덤으로 뽑아서 가이드 가져오기
+        return List.of(null);
+    }
+
+    public List<Region> getRecommendedRegion() {
+        // 로직 추가 필요
+        return List.of(null);
+    }
 }
