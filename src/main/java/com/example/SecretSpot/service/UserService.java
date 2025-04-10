5b594aadabfbb94a21b446ad1c798b9ffc65c8c2
ruddmslee <dlruddms326@naver.com>
2025-04-10 16:43:39 +0900

    Feat: 마이페이지 프로필 조회 기능

--- src/main/java/com/example/SecretSpot/service/UserService.java
+++ src/main/java/com/example/SecretSpot/service/UserService.java
@@ -1,10 +1,32 @@
 package com.example.SecretSpot.service;
 
+import com.example.SecretSpot.domain.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+//    private final GuideRepository guideRepository;
+//    private final RankingRepository rankingRepository;
+//    private final ReviewRepository reviewRepository;
+//    private final KeyWordRepository keyWordRepository;
 
+    public Map<String, Object> getUserProfile(User user) {
+        //Long userId = user.getId();
+        // guide, review, keyword Repo에서 findByUserId(Long userId) List 반환하도록
+//        return Map.of("profile_image", user.getPicture(),
+//                "name", user.getName(),
+//                "nickname", user.getNickname(),
+//                "keyword", keyWordRepository.findByUserId(userId),
+//                "ranking", rankingRepository.findByUserId(userId).getRanking(),
+//                "point", rankingRepository.findByUserId(userId).getTotalPoint(),
+//                "userGuides", guideRepository.findByUserId(userId),
+//                "userReviews", reviewRepository.findByUserId(userId)
+//        );
+        return null;
+    }
 }
--- src/main/java/com/example/SecretSpot/web/controller/UserController.java
+++ src/main/java/com/example/SecretSpot/web/controller/UserController.java
@@ -1,4 +1,31 @@
 package com.example.SecretSpot.web.controller;
 
+import com.example.SecretSpot.domain.User;
+import com.example.SecretSpot.service.UserService;
+import com.nimbusds.oauth2.sdk.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+//    @GetMapping("/api/mypage/profile")
+//    public ResponseEntity<Map<String, Object>> getProfile(@AuthenticationPrincipal User user) {
+//        return ResponseEntity.ok(userService.getUserProfile(user));
+//    }
+
+//    @PatchMapping("/api/mypage/profile")
+//    public String updateProfile(@RequestBody Profile profile, @AuthenticationPrincipal User user) {
+//    }
 }
