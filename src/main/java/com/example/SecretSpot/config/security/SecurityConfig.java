f131f6fdb49dddf59652970c8615da4c3dab3e9e
Haneul Lee <145983374+tishakong@users.noreply.github.com>
2025-04-11 16:34:23 +0900

    Feature: GCP Cloud Run 배포 파일 (#13)

--- src/main/java/com/example/SecretSpot/config/security/SecurityConfig.java
+++ src/main/java/com/example/SecretSpot/config/security/SecurityConfig.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -26,11 +24,11 @@ public class SecurityConfig {
 
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/css/**", "/images/**", "/js/**").permitAll()
-                .anyRequest().authenticated())
+                        .requestMatchers("/", "/css/**", "/images/**", "/js/**", "/health").permitAll()
+                        .anyRequest().authenticated())
                 .addFilterBefore(new JwtAuthenticationFilter(jwtProvider, userService), UsernamePasswordAuthenticationFilter.class)
                 .oauth2Login(oauth -> oauth
-                                .successHandler(new OAuth2SuccessHandler(jwtProvider, userRepository)));
-            return http.build();
+                        .successHandler(new OAuth2SuccessHandler(jwtProvider, userRepository)));
+        return http.build();
     }
 }
