cc31ee22fd20c94d2e6e11d7089f78233c9b0b33
tishakong <leehn45@naver.com>
2025-04-16 16:57:48 +0900

    Fix: BaseTimeEntity 등에 @Getter 어노테이션 추가

--- src/main/java/com/example/SecretSpot/domain/common/BaseCreatedEntity.java
+++ src/main/java/com/example/SecretSpot/domain/common/BaseCreatedEntity.java
@@ -3,9 +3,11 @@ package com.example.SecretSpot.domain.common;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 public abstract class BaseCreatedEntity {
     @Column(updatable = false, nullable = false)
