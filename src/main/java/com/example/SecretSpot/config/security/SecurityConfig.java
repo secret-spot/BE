ab618a8db147d5fa8efbf8526db9cc339ced3413
tishakong <leehn45@naver.com>
2025-04-16 23:06:09 +0900

    Fix: OAuth2SuccessHandler 내 환경변수 미적용 오류 수정
    - .env에서 값을 설정하였는데도 OAuth2SuccessHandler의 redirectBaseUrl이 null값으로 적용되던 현상을 수정했습니다.
    - 핸들러에 @Component 어노테이션을 달아 Spring에서 Bean으로 관리할 수 있게 수정 후, SecurityConfig에서 직접 new로 선언하지 않고 스프링 컨텍스트 내에서 직접 주입받도록 수정했습니다.

--- src/main/java/com/example/SecretSpot/config/security/SecurityConfig.java
+++ src/main/java/com/example/SecretSpot/config/security/SecurityConfig.java
@@ -1,15 +1,12 @@
 package com.example.SecretSpot.config.security;
 
-import com.example.SecretSpot.repository.KeywordRepository;
-import com.example.SecretSpot.repository.RankingRepository;
-import com.example.SecretSpot.repository.UserKeywordRepository;
-import com.example.SecretSpot.repository.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -17,17 +14,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
-    private final JwtProvider jwtProvider;
-    private final UserRepository userRepository;
-    private final RankingRepository rankingRepository;
-    private final UserKeywordRepository userKeywordRepository;
-    private final KeywordRepository keywordRepository;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(AbstractHttpConfigurer::disable)
                 .exceptionHandling(exception -> exception
                         .authenticationEntryPoint((request, response, authException) -> {
                             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
@@ -37,9 +30,9 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/", "/css/**", "/images/**", "/js/**").permitAll()
                         .anyRequest().authenticated())
-                .addFilterBefore(new JwtAuthenticationFilter(jwtProvider, userRepository), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .oauth2Login(oauth -> oauth
-                        .successHandler(new OAuth2SuccessHandler(jwtProvider, userRepository, rankingRepository, userKeywordRepository, keywordRepository)));
+                        .successHandler(oAuth2SuccessHandler));
         return http.build();
     }
 }
