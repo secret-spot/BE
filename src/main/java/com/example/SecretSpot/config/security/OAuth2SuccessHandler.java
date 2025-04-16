b41d0df97c18446a34ea1e8738002bb994a60287
tishakong <leehn45@naver.com>
2025-04-16 23:06:09 +0900

    Fix: OAuth2SuccessHandler 내 환경변수 미적용 오류 수정
    - .env에서 값을 설정하였는데도 OAuth2SuccessHandler의 redirectBaseUrl이 null값으로 적용되던 현상을 수정했습니다.
    - 핸들러에 @Component 어노테이션을 달아 Spring에서 Bean으로 관리할 수 있게 수정 후, SecurityConfig에서 직접 new로 선언하지 않고 스프링 컨텍스트 내에서 직접 주입받도록 수정했습니다.

--- src/main/java/com/example/SecretSpot/config/security/OAuth2SuccessHandler.java
+++ src/main/java/com/example/SecretSpot/config/security/OAuth2SuccessHandler.java
@@ -16,10 +16,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
