7b76f5f78f172b7eb4d037ce2dda03ebca86df4d
tishakong <leehn45@naver.com>
2025-04-16 16:58:19 +0900

    Chore: UserUtils의 getNicknameOrName 함수 주석 추가

--- src/main/java/com/example/SecretSpot/common/util/UserUtils.java
+++ src/main/java/com/example/SecretSpot/common/util/UserUtils.java
@@ -5,6 +5,8 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class UserUtils {
+
+    //닉네임 존재 시 닉네임, 없을 경우 이름을 반환하는 함수
     public String getNicknameOrName(User user) {
         String nickname = user.getNickname();
         return (nickname != null) ? nickname : user.getName();
