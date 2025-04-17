d57a6f5129b4261f7e720935ee05af335dd9e362
Haneul Lee <145983374+tishakong@users.noreply.github.com>
2025-04-17 15:30:09 +0900

    Feat: 알려지지 않은 장소 가이드 상위 10개 반환 API 구현 (#21)
    - /api/guides/hidden 경로로 구현했습니다.
    - GuideListItemDto의 경우, 추후 스크랩한 가이드 리스트에도 동일한 DTO를 사용할 것으로 예상해 추후 재사용하기 위해 별도의 converter 함수를 만들어 구현해뒀습니다.

--- src/main/java/com/example/SecretSpot/repository/GuideImageRepository.java
+++ src/main/java/com/example/SecretSpot/repository/GuideImageRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GuideImageRepository extends JpaRepository<GuideImage, Long> {
     Boolean existsByGuideAndUrl(Guide guide, String Url);
+
+    GuideImage findTop1ByGuideIdOrderBySortOrderAsc(Long guideId);
 }
--- src/main/java/com/example/SecretSpot/repository/GuideRegionRepository.java
+++ src/main/java/com/example/SecretSpot/repository/GuideRegionRepository.java
@@ -6,6 +6,10 @@ import com.example.SecretSpot.domain.Region;
 import com.example.SecretSpot.domain.compositekeys.GuideRegionId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface GuideRegionRepository extends JpaRepository<GuideRegion, GuideRegionId> {
     boolean existsByGuideAndRegion(Guide guide, Region region);
+
+    List<GuideRegion> findAllById_guideIdIn(List<Long> guideIds);
 }
