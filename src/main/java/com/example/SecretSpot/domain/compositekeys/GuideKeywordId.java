c1554bbc30925b8fe90d94b17d08558b0ccff959
tishakong <leehn45@naver.com>
2025-04-23 15:26:16 +0900

    Refactor: 복합키 클래스 리팩토링
    - 복합키의 경우 프록시 객체로 사용할 일이 거의 없기 때문에 기존 Hibernate.getClass()로 복합키의 클래스를 비교하던 것을 getClass()로 수정했습니다.
    - 복합키에서 Setter 어노테이션을 삭제했습니다.
    - ScrapId, RegionKeywordId에 생성자가 없던 것을 추가했습니다.

--- src/main/java/com/example/SecretSpot/domain/compositekeys/GuideKeywordId.java
+++ src/main/java/com/example/SecretSpot/domain/compositekeys/GuideKeywordId.java
@@ -4,13 +4,10 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.hibernate.Hibernate;
 
 import java.util.Objects;
 
 @Getter
-@Setter
 @Embeddable
 @NoArgsConstructor
 public class GuideKeywordId implements java.io.Serializable {
@@ -29,7 +26,7 @@ public class GuideKeywordId implements java.io.Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        if (o == null || getClass() != o.getClass()) return false;
         GuideKeywordId entity = (GuideKeywordId) o;
         return Objects.equals(this.keywordId, entity.keywordId) &&
                 Objects.equals(this.guideId, entity.guideId);
