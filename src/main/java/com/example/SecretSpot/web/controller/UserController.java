85c04316cc76e527e7b2f06005d75c8208db0110
tishakong <leehn45@naver.com>
2025-05-04 22:59:10 +0900

    Feat: username 반환 API 구현

--- src/main/java/com/example/SecretSpot/web/controller/UserController.java
+++ src/main/java/com/example/SecretSpot/web/controller/UserController.java
@@ -1,13 +1,18 @@
 package com.example.SecretSpot.web.controller;
 
+import com.example.SecretSpot.common.util.UserUtils;
 import com.example.SecretSpot.domain.User;
 import com.example.SecretSpot.service.UserService;
 import com.example.SecretSpot.web.dto.GuideCardItemDto;
 import com.example.SecretSpot.web.dto.ProfileUpdateRequestDto;
+import com.example.SecretSpot.web.dto.UserNameDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -35,5 +40,11 @@ public class UserController {
     @GetMapping("/api/mypage/guides")
     public ResponseEntity<List<GuideCardItemDto>> getGuides(@AuthenticationPrincipal User user) {
         return ResponseEntity.ok(userService.getUserGuides(user));
+    }
+
+    @GetMapping("/api/users/username")
+    public ResponseEntity<UserNameDto> getMyNicknameOrName(@AuthenticationPrincipal User user) {
+        UserNameDto responseDto = UserNameDto.builder().userName(UserUtils.getNicknameOrName(user)).build();
+        return ResponseEntity.ok(responseDto);
     }
 }
